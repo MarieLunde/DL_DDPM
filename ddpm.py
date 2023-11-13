a9ef08da6faf3ea0e8731b82a0bed52efa9c7b57
unknown <s183195@student.dtu.dk>
2023-11-13 18:56:57 +0100

    I fucked somthing up

--- ddpm.py
+++ ddpm.py
@@ -5,26 +5,28 @@ from torch.nn.functional import softplus
 from torch.distributions import Distribution
 #TODO: add utility class for DDPM
 
+def beta_scheduler(self):    
+    # Betas 
+    return torch.linspace(self.beta_start, self.beta_end, self.T, dtype = torch.float64) 
+
+
 class DDPM:
-    
+  
     def __init__(self, beta_start = 0.0001, beta_end = 0.02, T = 3):
         
         self.T = T # Timesteps
         self.beta_start = beta_start
         self.beta_end = beta_end
-     
-    def beta_scheduler(self):    
-        # Betas 
-        return torch.linspace(self.beta_start, self.beta_end, self.T, dtype = torch.float64) 
+        self.beta = beta_scheduler(self)  
+         
     
     def alphas(self):
-    # Alphas
+    # Alphas (???)
         self.alpha = 1 - self.beta
         self.alpha_bar = torch.cumprod(self.alpha, dim=0)
         self.alpha_bar_prev = np.append(1., self.alpha_bar[:-1])
         assert self.alpha_bar_prev.shape == (self.T,)
-        # return 
-    
+        # return   
 
     def noise_function(x0, t):
         # torch.rand_like(something) = Returns a tensor with the same size as input that is filled with random numbers from a normal distribution with mean 0 and variance 1.     
