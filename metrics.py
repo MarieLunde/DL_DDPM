f9ef3c9e2066bc39a8f4729dfc92f34fe5f8248e
ElineBrunke <s183195@student.dtu.dk>
2023-11-29 12:08:00 +0100

    inceptionscore added

--- metrics.py
+++ metrics.py
@@ -1,9 +1,13 @@
 import torch
 #from torchmetrics.functional import inception_score as tm_inception_score
 from torchmetrics.image.fid import FrechetInceptionDistance
+from torchmetrics.image.inception import InceptionScore
 
 
 def fid_score(real_img_samples, generated_img_samples):
+    
+    real_img_samples = real_img_samples.type(torch.uint8)
+    generated_img_samples = generated_img_samples.type(torch.uint8)
     
     fid = FrechetInceptionDistance(feature=2048)
 
@@ -12,3 +16,14 @@ def fid_score(real_img_samples, generated_img_samples):
     fidscore = fid.compute()
 
     return fidscore
+
+def inception_score(generated_img_samples):
+    
+    generated_img_samples = generated_img_samples.type(torch.uint8)
+    
+    inception = InceptionScore()
+    
+    inception.update(generated_img_samples)
+    inceptionscore = inception.compute()
+
+    return inceptionscore
