a84620c638f9a2e4e885933e5ab9a92670700b96
Marie Lunde <mao.lunde@gmail.com>
2023-11-29 12:56:13 +0100

    pull chanegs

--- dataloader.py
+++ dataloader.py
@@ -19,7 +19,7 @@ def get_dataloader(dataset_name, batch_size):
         dataset = datasets.MNIST(root=r"\data", download=True, train=True, transform=transform)
     elif dataset_name== "CIFAR10":
         transform = transforms.Compose([
-            transforms.RandomHorizontalFlip(p = 0.1),    # Randomly flip the image horizontally
+            transforms.RandomHorizontalFlip(p = 0.5),    # Randomly flip the image horizontally
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))
         ])
