e91e736a8c356a8b823a4aadfe88c04a4cbb4a0b
Anna Reisz <reiszanna08@gmail.com>
2023-12-04 20:00:42 +0100

    noising example for poster

---
+++ run_eval.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#BSUB -q gpuv100 
+#BSUB -J train
+#BSUB -o outs/eval_%J.out
+#BSUB -n 4
+#BSUB -R "rusage[mem=5GB]"
+#BSUB -W 04:00
+#BSUB -gpu "num=1:mode=exclusive_process"
+
+module load python3/3.11.3
+module load cuda/12.1.1
+source ~/irishcream/bin/activate
+
+JID=${LSB_JOBID}
+python eval.py
